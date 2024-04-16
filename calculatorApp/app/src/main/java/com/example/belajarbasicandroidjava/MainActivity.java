6c4a5ce571455606f9f8d5b7a52d40d8a41c1753
RaihanAthallah <rraihan1947@gmail.com>
2024-04-16 19:33:53 +0700

    sqlite app

--- calculatorApp/app/src/main/java/com/example/belajarbasicandroidjava/MainActivity.java
+++ calculatorApp/app/src/main/java/com/example/belajarbasicandroidjava/MainActivity.java
@@ -20,91 +20,45 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
 
         editTextNumber1 = findViewById(R.id.editTextNumber1);
         editTextNumber2 = findViewById(R.id.editTextNumber2);
         resultTextView = findViewById(R.id.resultTextView);
 
-        Button addButton = findViewById(R.id.add);
-        addButton.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v){
-                int num1 = Integer.parseInt(editTextNumber1.getText().toString());
-                int num2 = Integer.parseInt(editTextNumber2.getText().toString());
-                int sum = num1 + num2;
-
-                // Display the result (You can modify this according to your requirement)
-//                Toast.makeText(MainActivity.this, "Sum is: " + sum, Toast.LENGTH_SHORT).show();
-                resultTextView.setText(""+sum);
-            }
-        });
-
-        Button substractButton = findViewById(R.id.substract);
-        substractButton.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v){
-                int sum;
-                int num1 = Integer.parseInt(editTextNumber1.getText().toString());
-                int num2 = Integer.parseInt(editTextNumber2.getText().toString());
-                if (num2 > num1) {
-                    sum = num2 - num1;
-                } else {
-                    sum = num1 - num2;
-                }
-
-                // Display the result (You can modify this according to your requirement)
-//                Toast.makeText(MainActivity.this, "Sum is: " + sum, Toast.LENGTH_SHORT).show();
-                resultTextView.setText(""+sum);
-            }
-        });
-
-        Button multiplyButton = findViewById(R.id.multiply);
-        multiplyButton.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v){
-                float sum;
-                int num1 = Integer.parseInt(editTextNumber1.getText().toString());
-                int num2 = Integer.parseInt(editTextNumber2.getText().toString());
-
-                sum = num1 * num2;
-
-                // Display the result (You can modify this according to your requirement)
-//                Toast.makeText(MainActivity.this, "Sum is: " + sum, Toast.LENGTH_SHORT).show();
-                resultTextView.setText(""+sum);
-            }
-        });
-
-        Button divideButton = findViewById(R.id.divide);
-        divideButton.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v){
-                float sum;
-                float num1 = Float.parseFloat(editTextNumber1.getText().toString());
-                float num2 = Float.parseFloat(editTextNumber2.getText().toString());
-
-                sum = num1 / num2;
-
-                // Display the result (You can modify this according to your requirement)
-//                Toast.makeText(MainActivity.this, "Sum is: " + sum, Toast.LENGTH_SHORT).show();
-                resultTextView.setText(""+sum);
-            }
-        });
-
-
-//        @Override
-//        protected void calculateNumbers(num1 int, num2 int){
-//
-//        }
+        findViewById(R.id.add).setOnClickListener(v -> performOperation('+'));
+        findViewById(R.id.substract).setOnClickListener(v -> performOperation('-'));
+        findViewById(R.id.multiply).setOnClickListener(v -> performOperation('*'));
+        findViewById(R.id.divide).setOnClickListener(v -> performOperation('/'));
     }
 
-
-
-
+    private void performOperation(char operator) {
+        try {
+            int num1 = Integer.parseInt(editTextNumber1.getText().toString());
+            int num2 = Integer.parseInt(editTextNumber2.getText().toString());
+            int result = 0;
+            switch (operator) {
+                case '+':
+                    result = num1 + num2;
+                    break;
+                case '-':
+                    result = num1 - num2;
+                    break;
+                case '*':
+                    result = num1 * num2;
+                    break;
+                case '/':
+                    if (num2 == 0) {
+                        Toast.makeText(MainActivity.this, "Cannot divide by zero", Toast.LENGTH_SHORT).show();
+                        return;
+                    } else {
+                        result = num1 / num2;
+                    }
+                    break;
+            }
+            resultTextView.setText(String.valueOf(result));
+        } catch (NumberFormatException e) {
+            Toast.makeText(MainActivity.this, "Invalid input", Toast.LENGTH_SHORT).show();
+        }
+    }
 }
