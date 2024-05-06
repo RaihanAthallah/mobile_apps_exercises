e7bfd929a0ae20fa5bfcf581dd64527c8a7bcdb6
RaihanAthallah <rraihan1947@gmail.com>
2024-05-06 10:28:06 +0700

    Finish JsonParserApp

--- cameraApp/app/src/main/java/com/example/cameraapp/MainActivity.java
+++ cameraApp/app/src/main/java/com/example/cameraapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.cameraapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -7,9 +8,31 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.graphics.Bitmap;
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+
+import android.net.Uri;
+import android.os.Environment;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btn;
+    ImageView img;
+
+    ActivityResultLauncher<Intent> cameraLauncher;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +43,53 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        btn = (Button) findViewById(R.id.button);
+        img = (ImageView) findViewById(R.id.imageView);
+
+        // Initialize the ActivityResultLauncher
+        cameraLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
+            @Override
+            public void onActivityResult(ActivityResult result) {
+                if (result.getResultCode() == RESULT_OK && result.getData() != null) {
+                    Bitmap imageBitmap = (Bitmap) result.getData().getExtras().get("data");
+                    img.setImageBitmap(imageBitmap);
+                    saveImageToGallery(imageBitmap);
+                }
+            }
+        });
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                cameraLauncher.launch(intent);
+            }
+        });
+    }
+    private void saveImageToGallery(Bitmap bitmap) {
+        // Define the directory and filename where the image will be saved
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault()).format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + ".jpg";
+        File storageDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+
+        try {
+            // Create the file in the Pictures directory
+            File imageFile = new File(storageDir, imageFileName);
+
+            // Write the Bitmap to the file
+            OutputStream fos = new FileOutputStream(imageFile);
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, fos);
+            fos.flush();
+            fos.close();
+
+            // Notify the MediaStore about the newly added image
+            Uri imageUri = Uri.fromFile(imageFile);
+            Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+            mediaScanIntent.setData(imageUri);
+            sendBroadcast(mediaScanIntent);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
